2176e8b311a3d243c3f1558beb45cdb5ca7ab18e
rgillooly <rpgillooly@gmail.com>
2023-10-02 21:23:49 -0400

    altered html and added more js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
-function test() {
-fetch("developer.nps.gov/api/v1/parks")
+var searchInputVal = document.querySelector('#search').value;
+var searchButton = document.querySelector('#search-button');
+
+searchButton.addEventListener("click", parksInState);
+
+function parksInState(searchInputVal) {
+console.log(searchInputVal);
+// curl -X GET "https://developer.nps.gov/api/v1/parks?stateCode=" + searchInputVal + "&limit=50&start=0&q=address" -H "accept: application/json";
 }
 
