d7bb8e82fe6972b216bd8b177eafb09f25677cf7
ommp7 <132945305+ommp7@users.noreply.github.com>
2023-10-05 18:10:36 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,49 @@
-var searchInputVal = document.querySelector('#search').value;
-var searchButton = document.querySelector('#search-button');
+// Constants
+const apiKey = "a5TRtoX59To11woolQUJEs2WY34dc0AZ6MppUzci";
+const openWeatherMapApiKey = '876fe47417eaaeff0f787d1ddd261473';
 
-searchButton.addEventListener("click", parksInState);
+// DOM Elements
+const searchBtn = document.getElementById("search-button");
+const parkDisplayContainer = document.getElementById("park-display-container");
 
-function parksInState(searchInputVal) {
-console.log(searchInputVal);
-// curl -X GET "https://developer.nps.gov/api/v1/parks?stateCode=" + searchInputVal + "&limit=50&start=0&q=address" -H "accept: application/json";
+// Event Listeners
+searchBtn.addEventListener("click", fetchParkData);
+window.addEventListener('load', displayStoredParkInfo);
+
+function fetchParkData() {
+    // ... (Your existing fetchParkData function)
+
+    // Event listeners for modal close
+    const closeModalBtn = document.getElementById("noMatchingParkModal");
+    closeModalBtn.addEventListener("click", () => closeModal("noMatchingParkModal"));
+    window.addEventListener("click", (event) => {
+        if (event.target === closeModalBtn) closeModal("noMatchingParkModal");
+    });
 }
+
+function fetchWeatherForecast(latitude, longitude) {
+    // ... (Your existing fetchWeatherForecast function)
+}
+
+function displayParkInfo(park) {
+    // ... (Your existing displayParkInfo function)
+}
+
+function displayStoredParkInfo() {
+    // ... (Your existing displayStoredParkInfo function)
+}
+
+function displayWeatherForecast(weatherData) {
+    // ... (Your existing displayWeatherForecast function)
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+function clearParkDisplay() {
+    parkDisplayContainer.innerHTML = "";
+}
+
 
